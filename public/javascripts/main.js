6ff908cb7925d7196c6f36704f2f6cc4784bc10a
Luke Stebner <luke.stebner@gmail.com>
2014-05-29 22:31:10 -0700

    trying to fix tbn

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,11 +6,14 @@ var team_name = ""
     ,team_data = {}
     ,today_data = null
     ,games_feed = '/games-data'
-    ,tbn = function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}
     ,World = {}
 ;
 
 $(function(){
+    if (typeof(tbn) == "undefined"){
+        tbn = function(){};
+    }
+
     team_name = $('input[name=team_name]').val();
     games_feed = $('input[name=games_url]').val();
     team_data = JSON.parse($('input[name=team_data]').val());
